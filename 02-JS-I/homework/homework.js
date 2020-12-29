636e16fe4a014b355ceb0aee7323dd3d72b3dd6a
luis3664 <luis.lopez3664@gmail.com>
2020-12-29 16:10:45 -0400

    tarea de la tercera clase JS II

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -65,7 +65,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x==y){
+  if (x===y){
     return true;
   }
   return false;
@@ -75,7 +75,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.length==str2.length){
+  if (str1.length===str2.length){
         return true;
       }
       return false;
@@ -112,7 +112,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num%2==0){
+  if (num%2===0){
     return true;
   }
   return false;
@@ -122,7 +122,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num%2!==0){
+  if (num%2!===0){
     return true;
   }
   return false;
